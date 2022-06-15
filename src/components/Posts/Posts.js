d81fcd8a71f8e8f464c16f532e9255aea965e91d
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-06-14 20:51:37 -0400

    Finished

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,7 +9,11 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(function (value) {
+        return <Post likePost={likePost} post={value} Key={value.id} />
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
+
     </div>
   );
 };
